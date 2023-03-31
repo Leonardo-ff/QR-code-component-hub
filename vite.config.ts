cf99d2e0d1599f6b9e499b8cbaffa7ee8e74ff46
Leonardo Freitas TC <leonardo-freitas@testingcompany.com.br>
2023-03-31 13:43:09 -0300

    Ajustando caminhos

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/QR-code-component-hub/',
+  base: '/qr-code-component-hub/',
   plugins: [react()],
 })
